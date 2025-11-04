bf318d08134e63e3c87de31fd6adeb4186f70767
filzia-web <filzia_maritza_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 12:46:35 +0700

    Add files via upload

---
+++ Ukl1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Ukl1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int biaya,berat,jarak,panjang,lebar,tinggi,volume,harga;
+
+        Scanner input = new Scanner (System.in);
+        System.out.println("Masukkan berat paket");
+        berat = input.nextInt();
+        System.out.println("Masukkan jarak pengiriman");
+        jarak  = input.nextInt();
+    
+
+        if (jarak <= 10) {
+            biaya = berat*4250;
+        }else if(jarak > 10){
+            biaya = berat *6000;
+        }
+
+        System.out.println("Masukkan panjang paket");
+        panjang = input.nextInt();
+        System.out.println("Masukkan lebar paket");
+        lebar = input.nextInt();
+        System.out.println("Masukkan tinggi paket");
+        tinggi = input.nextInt();
+        volume = panjang*lebar*tinggi;
+
+        if(volume>100){
+            biaya = berat +=50000;
+        }else{
+            biaya = berat +=0;
+        }
+        System.out.println("Total yang dibayarkan "+biaya);
+    }
+}
